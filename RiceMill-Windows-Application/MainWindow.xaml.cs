7c63930b851913101c31a0f8997f27ab509e0d5e
VampishWolf <ashishmittal574@gmail.com>
2020-02-19 01:47:40 +0530

    Second Commit

--- RiceMill-Windows-Application/MainWindow.xaml.cs
+++ RiceMill-Windows-Application/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Text.RegularExpressions;
+/*using MessageBox = System.Windows.MessageBox;*/
 
 namespace RiceMill_Windows_Application
 {
@@ -24,31 +26,124 @@ namespace RiceMill_Windows_Application
 		public MainWindow()
 		{
 			InitializeComponent();
-
-			//this.Left = SystemParameters.PrimaryScreenWidth - this.Width;
-			//this.Top = SystemParameters.WorkArea.Bottom - this.Height;
-
+			
+		}
+		private void MainWindow_Loaded(object sender, EventArgs e)
+		{
 			DispatcherTimer timer = new DispatcherTimer();
 			timer.Tick += Timer_Tick;
 			timer.Start();
-			
+
 		}
 		void Timer_Tick(object sender, EventArgs e)
 		{
-			dateDisplayLabel.Content = DateTime.Now.ToString("dd/MM/yyyy	hh:mm:ss tt");
+			dateDisplayLabel.Content = DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt");
 		}
 
-		private void Button_Click(object sender, RoutedEventArgs e)
+		/*private void DataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+			if (e.RowIndex >= 0)
+			{
+
+				*//*DataGridViewRow row = this.DataGridView.Rows[e.RowIndex];*//*
+				DataGridViewRow row = DataGridView.SelectedRows[1];
+
+				bagsCountInput.Text = row.Cells["BagsCount"].Value.ToString();
+				totalWeightInput.Text = row.Cells["ItemName"].Value.ToString();
+				rateInput.Text = row.Cells["Weight"].Value.ToString();
+				moistAllowedInput.Text = row.Cells["Rate"].Value.ToString();
+				moistInput.Text = row.Cells["Moisture"].Value.ToString();
+
+			}
+
+		}*/
+
+		private void Add_Click(object sender, RoutedEventArgs e)
 		{
 			try
-			{
+			{				
 				int bagscount = Convert.ToInt32(bagsCountInput.Text);
-				DataGridView.Items.Add(bagscount);
+				float totalweight = Convert.ToInt32(totalWeightInput.Text);
+				float weight = totalweight / bagscount;
+				float rate = Convert.ToInt32(rateInput.Text);
+				float amount = bagscount * rate;
+				float moistallowed = Convert.ToInt32(moistAllowedInput.Text);
+				float moist = Convert.ToInt32(moistInput.Text);
+				float claim;				
+				if (moist <= 18){
+					claim = 0;
+				} else
+				{
+					claim = moist - moistallowed;
+				}
+				float claimant = (claim * amount) / 100;
+				float finalamount = (bagscount * rate) - claimant;
+				// DataGridView.Rows.Add( bagscount, itemNameInput.Text, weight, rate, moist, claim, amount );
+				DataGridView.Items.Add(new { BagsCount = bagscount, ItemName = itemNameInput.Text, Weight = weight, Rate = rate, Amt = amount, Moisture = moist, Claim = claim, Amount = finalamount });
+				// DataGridView.Items.Add(bagscount);
 			} catch (Exception ex)
 			{
 				MessageBox.Show(ex.Message.ToString());
 			}
 			
 		}
+		private void Update_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				int bagscount = Convert.ToInt32(bagsCountInput.Text);
+				float totalweight = Convert.ToInt32(totalWeightInput.Text);
+				float weight = totalweight / bagscount;
+				float rate = Convert.ToInt32(rateInput.Text);
+				float amount = bagscount * rate;
+				float moistallowed = Convert.ToInt32(moistAllowedInput.Text);
+				float moist = Convert.ToInt32(moistInput.Text);
+				float claim;
+				if (moist <= 18)
+				{
+					claim = 0;
+				}
+				else
+				{
+					claim = moist - moistallowed;
+				}
+				float claimant = (claim * amount) / 100;
+				float finalamount = (bagscount * rate) - claimant;
+				// DataGridView.Rows.Add( bagscount, itemNameInput.Text, weight, rate, moist, claim, amount );
+				DataGridView.Items.Add(new { BagsCount = bagscount, ItemName = itemNameInput.Text, Weight = weight, Rate = rate, Amt = amount, Moisture = moist, Claim = claim, Amount = finalamount });
+				// DataGridView.Items.Add(bagscount);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message.ToString());
+			}
+
+		}
+		private void Delete_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				var grid = DataGridView;
+				if (grid.SelectedIndex >= 0)
+				{
+					for (int i = 0; i <= grid.SelectedItems.Count; i++)
+					{
+						grid.Items.Remove(grid.SelectedItems[i]);
+					};
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message.ToString());
+			}
+
+		}
+		private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+		{
+			Regex regex = new Regex("[^0-9]+");
+			e.Handled = regex.IsMatch(e.Text);
+		}
+
+		
 	}
 }
